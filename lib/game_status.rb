7d84bdb6be7c532f45c69e4d17a085420c2f098f
Alice T <alicewtien@gmail.com>
2018-07-27 21:00:53 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -36,7 +36,7 @@ end
 def winner(board)
   WIN_COMBINATIONS.each do |combo|
     if won?(board)
-      return WIN_COMBINATIONS[combo[0]]
+      return board[combo[0]]
     else
       return nil
     end
