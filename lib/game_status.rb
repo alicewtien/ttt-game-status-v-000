c69dcf35a8356b937e2eb33e6b812a88b16d81da
Alice T <alicewtien@gmail.com>
2018-07-27 20:50:52 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -37,7 +37,7 @@ def winner(board)
   WIN_COMBINATIONS.each do |combo|
     if won?(board) && board[combo[0]] == "X"
       return "X"
-    elsif won?(board)
+    elsif won?(board) && board[combo[0]] != "X"
       return "O"
     else
       return nil
